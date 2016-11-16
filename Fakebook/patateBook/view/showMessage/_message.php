b5e63fc52747369bd7a65426639fc43cefeaeee4
Gael Cuminal <gael.cuminal@alumni.univ-avignon.fr>
2016-11-16 11:53:21 +0100

    message template

--- Fakebook/patateBook/view/showMessage/_message.php
+++ Fakebook/patateBook/view/showMessage/_message.php
@@ -1,4 +1,5 @@
-<!-- @author = Gael Cuminal -->
-<pre>
-<?= $message->post->texte ?> 
-</pre>
+<!-- @author = Gael CUminal -->
+<?php $post = postTable::getPostById($message->post) ?>
+<?php $emetteur = utilisateurTable::getUserById($message->emetteur) ?>
+<?php $parent = utilisateurTable::getUserById($message->parent) ?>
+<?= $post->texte ?> par <?= $emetteur->identifiant ?> à destination de $dest->identifiant (le parent étant : <?= $parent->identifiant ?>)
