1fb6ef2c88b04a66d5000af01cb311e3aa05cc90
Gael Cuminal <gael.cuminal@alumni.univ-avignon.fr>
2016-12-05 14:09:23 +0100

    layout

---
+++ Fakebook/patateBook/view/layout.php
@@ -0,0 +1,48 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+	<title>PatateBook</title>
+	<meta charset="utf-8">
+	<link rel="stylesheet" type="text/css" href="css/<?php echo $nameApp; ?>.css">
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.5/css/bootstrap.min.css" integrity="sha384-AysaV+vQoT3kOAXZkl02PThvDr8HYKPZhNT5h/CXfBThSRXQ6jW5DO2ekP5ViFdi" crossorigin="anonymous">
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
+	<script src="https://npmcdn.com/tether@1.2.4/dist/js/tether.min.js"></script>
+	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.5/js/bootstrap.min.js" integrity="sha384-BLiI7JTZm+JWlgKa0M0kGRpJbF2J8q+qreVrKBC47e3K6BW78kGLrCkeRX6I9RoK" crossorigin="anonymous"></script>
+</head>
+<body>
+<!-- @author = Gael Cuminal -->
+	<div class="notif-msg <?php if($context->notif != false) {echo 'in';} ?>"><?php echo $context->notif; ?></div>
+		<div class="container">
+			<div class="row">
+				<header class="col-xs-12">Ceci est le header</header>
+				<aside class="col-md-3 hidden-md-down">
+					Ici la liste utilisateurs
+					<button data-target="#chat" data-toggle="modal">avec un lien vers le modal de chat</button>
+				</aside>
+				<section class="col-xs-12 col-md-6">
+					Ici le contenu de la page
+				</section>
+			</div>
+	</div>
+
+	<div class="modal fade" id="chat">
+	 <div class="modal-dialog" role="document">
+	   <div class="modal-content">
+	     <div class="modal-header">
+	       <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+	         <span aria-hidden="true">&times;</span>
+	       </button>
+	       <h4 class="modal-title">Modal title</h4>
+	     </div>
+	     <div class="modal-body">
+	       <p>One fine body&hellip;</p>
+	     </div>
+	     <div class="modal-footer">
+	       <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+	       <button type="button" class="btn btn-primary">Save changes</button>
+	     </div>
+	   </div><!-- /.modal-content -->
+	 </div><!-- /.modal-dialog -->
+	</div><!-- /.modal -->
+</body>
+</html>
