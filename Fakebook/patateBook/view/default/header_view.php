937e57409891f4835609f932cc1d8da482ac031d
Gael Cuminal <gael.cuminal@alumni.univ-avignon.fr>
2016-12-05 15:35:11 +0100

    CSS and nav

---
+++ Fakebook/patateBook/view/default/header_view.php
@@ -0,0 +1,4 @@
+<nav class="nav nav-inline">
+	<a href="#" class="nav-link active">Accueil</a>
+	<a href="#" class="nav-link active">Mon profil</a>
+</nav>
--- Fakebook/patateBook/view/layout.php
+++ Fakebook/patateBook/view/layout.php
@@ -3,8 +3,12 @@
 <head>
 	<title>PatateBook</title>
 	<meta charset="utf-8">
-	<link rel="stylesheet" type="text/css" href="css/<?php echo $nameApp; ?>.css">
+
+	<!-- CSS -->
 	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.5/css/bootstrap.min.css" integrity="sha384-AysaV+vQoT3kOAXZkl02PThvDr8HYKPZhNT5h/CXfBThSRXQ6jW5DO2ekP5ViFdi" crossorigin="anonymous">
+	<link rel="stylesheet" type="text/css" href="css/<?php echo $nameApp; ?>.css">
+
+	<!-- SCRPITS -->
 	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
 	<script src="https://npmcdn.com/tether@1.2.4/dist/js/tether.min.js"></script>
 	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.5/js/bootstrap.min.js" integrity="sha384-BLiI7JTZm+JWlgKa0M0kGRpJbF2J8q+qreVrKBC47e3K6BW78kGLrCkeRX6I9RoK" crossorigin="anonymous"></script>
@@ -14,13 +18,12 @@
 	<div class="notif-msg <?php if($context->notif != false) {echo 'in';} ?>"><?php echo $context->notif; ?></div>
 		<div class="container">
 			<div class="row">
-				<header class="col-xs-12"><?php include $header_view ?></header>
+				<header class="col-xs-12"><?php include $header_view; ?></header>
 				<aside class="col-md-3 hidden-md-down">
-					<?php include $left_view ?>
-					<button data-target="#chat" data-toggle="modal">avec un lien vers le modal de chat</button>
+					<?php include $left_view; ?>
 				</aside>
-				<section class="col-xs-12 col-md-6">
-					<?php include $action_view ?>
+				<section class="col-xs-12 col-md-9">
+					<?php include $action_view; ?>
 				</section>
 			</div>
 	</div>
