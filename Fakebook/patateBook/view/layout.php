9eefd34dafcc0755a250d2943ed3ccaede8c621c
Gael Cuminal <gael.cuminal@alumni.univ-avignon.fr>
2016-12-05 13:59:28 +0100

    new layout

--- Fakebook/patateBook/view/layout.php
+++ Fakebook/patateBook/view/layout.php
@@ -10,8 +10,17 @@
 <body>
 <!-- @author = Gael Cuminal -->
 	<div class="notif-msg <?php if($context->notif != false) {echo 'in';} ?>"><?php echo $context->notif; ?></div>
-	<h2> Bienvenue sur <?php echo ucfirst($nameApp); ?> </h2>
-	<?php include($template_view); ?>
-    
+		<div class="container">
+			<div class="row">
+				<header class="col-xs-12">Ceci est le header</header>
+				<aside class="col-md-3 hidden-md-down">
+					Ici la liste utilisateurs
+					avec un lien vers le modal de chat
+				</aside>
+				<section class="col-xs-12 col-md-6">
+					Ici le contenu de la page
+				</section>
+			</div>
+	</div>    
 </body>
 </html>
