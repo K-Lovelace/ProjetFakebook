06075e87d839d459a253b4abc9c0347e46f775d5
Gael Cuminal <gael.cuminal@alumni.univ-avignon.fr>
2016-12-11 23:36:21 +0100

    init chat

--- Fakebook/patateBook/view/default/header_view.php
+++ Fakebook/patateBook/view/default/header_view.php
@@ -1,5 +1,6 @@
 <nav class="nav nav-inline">
 	<a href="?action=index" class="nav-link active">Accueil</a>
-	<a href="?action=profile" class="nav-link active">Mon profil</a>
-	<a data-api="logout" class="nav-link active">Se déconnecter</a>
+	<a href="?action=profile" class="nav-link">Mon profil</a>
+	<a data-api="logout" class="nav-link">Se déconnecter</a>
+	<a href="#" class="nav-link chat-notifier">Afficher le chat<span class="chat-count">3</span></a>
 </nav>
