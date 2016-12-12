d25facb0a40ab95424f2e88b4addd876fa17af5d
Gael Cuminal <gael.cuminal@alumni.univ-avignon.fr>
2016-12-12 08:30:01 +0100

    Header

--- Fakebook/patateBook/view/default/header_view.php
+++ Fakebook/patateBook/view/default/header_view.php
@@ -1,5 +1,8 @@
 <nav class="nav nav-inline">
 	<a href="?action=index" class="nav-link active">Accueil</a>
-	<a href="?action=profile" class="nav-link active">Mon profil</a>
-	<a data-api="logout" class="nav-link active">Se déconnecter</a>
+	<?php if($context->getSessionAttribute("current_user"))?>
+		<a href="?action=profile" class="nav-link active">Mon profil</a>
+		<a data-api="logout" class="nav-link active">Se déconnecter</a>
+	<?php else ?>
+	<?php endif; ?>
 </nav>
---
+++ Fakebook/patateBook/view/profile/success.php
@@ -0,0 +1,8 @@
+<div class="row">
+	<div class="col-xs-12">
+		<?php include $nameApp.'/view/shared/_user.php'; ?>
+	</div>
+	<div class="col-xs-12">
+		<?php include $nameApp.'/view/shared/_wall.php'; ?>		
+	</div>
+</div>
