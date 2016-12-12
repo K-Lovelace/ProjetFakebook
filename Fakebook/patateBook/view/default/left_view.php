094675378a75ec6f56acd490ff99eec087118330
Rudelou Pierre <uapv1702873@pedago02a.univ-avignon.fr>
2016-12-12 16:23:57 +0100

    CSS CSS CSS git add .git add .git add .git add .git add .git add .git add .git add .git add .git add .git add .git add .git add .git add .git add .git add .git add .git add .git add .git add ..jpp

--- Fakebook/patateBook/view/default/left_view.php
+++ Fakebook/patateBook/view/default/left_view.php
@@ -3,9 +3,11 @@
 <div class="row">
 	<?php $user = $context->getSessionAttribute("currentUser") ?>
 	<?php if($user != false) { ?>
-		<div class="user-profile col-xs-12">
+		<div class="col-xs-12 current_user_identity_panel ">
 			<?php $user_class = 'thumbnail-perso'?>
 			<?php include ($nameApp."/view/shared/_user.php") ?>
+			<?php $user_class = NULL?>
+
 		</div>
 	<?php } ?>
 
