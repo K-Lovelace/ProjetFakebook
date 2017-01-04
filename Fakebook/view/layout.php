9c7dd575c57b763b5c27bc9ae4c47f55bc6ec7bb
allonzy <simviv28@hotmail.fr>
2017-01-04 16:42:39 +0100

    corrected layout

--- Fakebook/view/layout.php
+++ Fakebook/view/layout.php
@@ -26,7 +26,6 @@
 <?php } ?>
 <div class="container">
     <div class="row" id="content">
-        BITE
         <header class="col-xs-12"><?php include $header_view; ?></header>
         <aside class="col-md-3 hidden-md-down">
             <?php include $left_view; ?>
