347b9415895e08538f2a37de57b36dbedeb9bf62
allonzy <simviv28@hotmail.fr>
2017-01-13 18:49:14 +0100

    stuff

--- Fakebook/view/default/header_view.php
+++ Fakebook/view/default/header_view.php
@@ -3,7 +3,7 @@
 	<a href="?action=index" class="nav-link ajax-link">Accueil</a>
 	<?php if(NULL !== $context->getSessionAttribute("currentUser")) {?>
 		<a href="?action=profile" class="nav-link ajax-link">Mon profil</a>
-		<a href="#" class="nav-link">Se déconnecter</a>
+		<a href="?action=logout" class="nav-link">Se déconnecter</a>
 		<a href="#" data-toggle="modal" data-target="#chat" class="ajax-link nav-link chat-notifier">Afficher le chat<span class="chat-count">3</span></a>
 	<?php }  else { ?>
 		<a href="?action=login" class="nav-link pull-xs-right"> Connexion </a>
