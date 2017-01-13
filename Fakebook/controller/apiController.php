48550eab4fa7f8f51838c5cec26a763fe139b720
allonzy <simviv28@hotmail.fr>
2017-01-13 15:21:33 +0100

    ajax profile

--- Fakebook/controller/apiController.php
+++ Fakebook/controller/apiController.php
@@ -8,6 +8,29 @@ class mainController
         $context->setSessionAttribute('currentUser', false);
         return context::SUCCESS;
     }
+
+    // author = Gael Cuminal
+    public static function profile($request, $context)
+    {
+        if ($context->getSessionAttribute("currentUser") === NULL) {
+            //User not signed in
+            $context->notif = "You are not signed in!";
+            return $context::ERROR;
+        }
+        $user = false;
+        if (isset($request['user']))
+            $user = utilisateurTable::getUserById($request["user"]);
+        else
+            $user = utilisateurTable::getUserById($context->getSessionAttribute('currentUser'));
+        var_dump($user);
+        if ($user === false) {
+            $context->notif = "User was not found";
+            return $context::ERROR;
+        }
+
+        $context->user = $user;
+        return $context::SUCCESS;
+    }
 }
 
 ?>
