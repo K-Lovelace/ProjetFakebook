9ea69ffb969dcb302410754f6a739ff9522219ed
allonzy <simviv28@hotmail.fr>
2017-01-13 11:45:42 +0100

    debug

--- Fakebook/view/default/header_view.php
+++ Fakebook/view/default/header_view.php
@@ -1,10 +1,11 @@
+<!--Author = Gael Cuminal-->
 <nav class="nav nav-inline">
 	<a href="?action=index" class="nav-link">Accueil</a>
-	<?php if(false !== $context->getSessionAttribute("currentUser")) {?>
+	<?php if(NULL !== $context->getSessionAttribute("currentUser")) {?>
 		<a href="?action=profile" class="nav-link ajax-link">Mon profil</a>
 		<a href="#" class="nav-link">Se déconnecter</a>
 		<a href="#" data-toggle="modal" data-target="#chat" class="ajax-link nav-link chat-notifier">Afficher le chat<span class="chat-count">3</span></a>
 	<?php }  else { ?>
-		<a href="?action=login" class="pull-xs-right"> Connexion </a>
+		<a href="?action=login" class="nav-link pull-xs-right"> Connexion </a>
 	<?php } ?>
 </nav>
